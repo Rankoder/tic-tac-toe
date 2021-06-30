9267353c50f1ed7c1d49c5dfab0b64f027cbf045
akronder@gmail.com <akronder@gmail.com>
2021-06-30 12:19:46 +0200

    add history of moves

--- src/index.js
+++ src/index.js
@@ -14,49 +14,18 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        }
-    }
-
-    handleClick(fieldNumber) {
-        const tempSquares = this.state.squares.slice();
-        if(calculateWinner(tempSquares) || tempSquares[fieldNumber]) {
-            return;
-        }
-        
-        tempSquares[fieldNumber] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: tempSquares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-
     renderSquare(fieldNumber) {
         return (
             <Square
-                value={this.state.squares[fieldNumber]}
-                onClick={() => this.handleClick(fieldNumber)}
+                value={this.props.squares[fieldNumber]}
+                onClick={() => this.props.onClick(fieldNumber)}
             />
         );
     }
 
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status = 'Następny gracz: ' + (this.state.xIsNext ? 'X' : 'O');
-
-        if (winner) {
-            status = 'Wygrywa: ' + winner;
-        } else {
-            status = 'Następny gracz: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -78,19 +47,82 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext:true,
+            stepNumber: 0,
+        };
+    }
+
+    handleClick(fieldNumber) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const tempSquares = current.squares.slice();
+
+        if(calculateWinner(tempSquares) || tempSquares[fieldNumber]) {
+            return;
+        }
+        
+        tempSquares[fieldNumber] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: tempSquares,
+            }]),            
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length,
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        })
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+        const moves = history.map((step, move) => {
+            const desc = move ? 
+                'Przejdz do ruchu #' + move :
+                'Przejdz na początek gry';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
+        let status;
+        
+        if (winner) {
+            status = 'Wygrywa: ' + winner;
+        } else {
+            status = 'Następny gracz: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board 
+                        squares={current.squares}
+                        onClick={(fieldNumber) => this.handleClick(fieldNumber)}
+                    />
                 </div>
                 <div className="game-info">
-                    {/* <div>statusy</div> */}
-                    {/* <ol>TODO</ol> */}
+                    {<div>{status}</div>}
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
     }
+
 }
 
 // ===================================================
